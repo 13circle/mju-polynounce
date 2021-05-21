7a085c4799e6176f7785da53318adaf315067bbb
13circle <13circle97@gmail.com>
2021-05-21 23:46:46 +0900

    Define undefined option: extended

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ app.set("view engine", "ejs");
 app.engine("html", ejs.renderFile);
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.text());
 
 app.use("/static", express.static(path.resolve("src", "static")));
