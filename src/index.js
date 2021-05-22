45c62362588ca073581ccc2e9959926a8356cbb6
13circle <13circle97@gmail.com>
2021-05-23 02:41:48 +0900

    Apply express-session and its store modules - if DB connected via SSH -> session-file-store will be chosen - if DB connected normally -> express-mysql-session will be chosen

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const morgan = require("morgan");
 const path = require("path");
 const ejs = require("ejs");
 
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const MySQLStore = require("express-mysql-session")(session);
+
 dotenv.config();
 
 const { PORT, NODE_ENV } = process.env;
@@ -17,11 +21,62 @@ const app = express();
 
 const routes = require("./routes");
 
+const MyDBConnector = require("./models/MyDBConnector");
+
 const handleRouteError = require("./lib/mw/handle-route-error");
 
 if (NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+async function connectDB(callback) {
+  global.db = new MyDBConnector();
+
+  const { db } = global;
+
+  try {
+    await db.initPool();
+
+    setInterval(async () => {
+      await db.refreshPool();
+      if (NODE_ENV === "development") {
+        console.log("MySQL Pool Refreshed");
+      }
+    }, 1000 * 60 * 15);
+
+    if (callback)
+      callback(
+        db.isUsingSSH()
+          ? db.getMysqlConfig()
+          : {
+              path: path.resolve(__dirname, "sessions"),
+            }
+      );
+  } catch (err) {
+    throw err;
+  }
+}
+connectDB((storeConfig) => {
+  const { SESSION_SECRET } = process.env;
+
+  if (!SESSION_SECRET) {
+    throw Error("SESSION_SECRET must be specified");
+  }
+
+  const store = db.isUsingSSH()
+    ? new FileStore(storeConfig)
+    : new MySQLStore(storeConfig);
+
+  app.use(
+    session({
+      key: "MJU_POLYNOUNCE_SESSION",
+      secret: SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      store,
+    })
+  );
+});
 
 app.set("views", path.resolve(__dirname, "view"));
 app.set("view engine", "ejs");
