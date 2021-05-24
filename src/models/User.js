118ae25857ddc7687cc33faa485872c4350fadc5
13circle <13circle97@gmail.com>
2021-05-24 17:38:00 +0900

    Add User model

---
+++ src/models/User.js
@@ -0,0 +1,114 @@
+const { DataTypes, Model } = require("sequelize");
+
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+const saltRounds = 13;
+
+const { PASSWD_SECRET } = process.env;
+
+if (!PASSWD_SECRET) {
+  throw Error("PASSWD_SECRET must be specified");
+}
+
+class User extends Model {
+  serialize() {
+    const user = this.toJSON();
+
+    delete user.userPwd;
+    delete user.studPwd;
+    delete user.emailCheckCode;
+
+    return user;
+  }
+
+  async hashUserPassword(userPwd) {
+    this.userPwd = await bcrypt.hash(userPwd, saltRounds);
+  }
+
+  encryptStudPassword(studPwd) {
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv(
+      "aes-256-cbc",
+      Buffer.from(PASSWD_SECRET),
+      iv
+    );
+
+    const enc = cipher.update(studPwd);
+
+    this.studPwd =
+      iv.toString("hex") +
+      ":" +
+      Buffer.concat([enc, cipher.final()]).toString("hex");
+  }
+
+  decryptStudPassword() {
+    const strs = this.studPwd.split(":");
+    const iv = Buffer.from(strs.shift(), "hex");
+    const enc = Buffer.from(strs.join(":"), "hex");
+    const decipher = crypto.createDecipheriv(
+      "aes-256-cbc",
+      Buffer.from(PASSWD_SECRET),
+      iv
+    );
+
+    const dec = decipher.update(enc);
+    const studPwd = Buffer.concat([dec, decipher.final()]).toString();
+
+    this.encryptStudPassword(studPwd);
+
+    return studPwd;
+  }
+}
+
+function initModel(sequelize) {
+  User.init(
+    {
+      userEmail: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      userPwd: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      studId: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      studPwd: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      mjuEmail: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      isUserEmailVerified: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      isMjuEmailVerified: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      emailCheckCode: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: () => Math.random().toString(36).slice(2),
+      },
+    },
+    {
+      modelName: "User",
+      sequelize,
+    }
+  );
+}
+
+module.exports = {
+  User,
+  initModel,
+};
