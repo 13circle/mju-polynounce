36cd8e0f395808e2da7daab081f6c0d7f10501ad
13circle <13circle97@gmail.com>
2021-06-13 08:44:22 +0900

    Handle login status by using callback

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,5 +1,43 @@
 var execOnReady = [];
 
+function setLoginStatus() {
+  $(".link-btn").each((i, e) => {
+    $(e).toggleClass("inactive");
+  });
+  $(".for-non-user").hide();
+  $(".for-user").show();
+}
+
+function setNonLoginStatus() {
+  $(".for-non-user").show();
+  $(".for-user").hide();
+}
+
+function checkLogin(noParamCallback) {
+  $.ajax({
+    url: "/auth/check",
+    method: "get",
+    dataType: "json",
+    success: function (data) {
+      if (data.isLogin) {
+        setLoginStatus();
+        noParamCallback();
+      } else {
+        setNonLoginStatus();
+      }
+    },
+  });
+}
+
+execOnReady.push(function () {
+  $(".logout").click(function () {
+    $.ajax({
+      url: "/auth/logout",
+      method: "delete",
+    });
+  });
+});
+
 $(document).ready(function () {
   for (var i in execOnReady) {
     if (typeof execOnReady[i] === "function") {
